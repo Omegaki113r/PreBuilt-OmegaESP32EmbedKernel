6461aee11df3a10eec8f49fbfddd9ef900221599
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-06-01 00:00:21 +0530

    initial build

---
+++ OmegaEmbedKernel/OmegaEmbedKernel.h
@@ -0,0 +1,35 @@
+/**
+ * @file OmegaEmbedKernel.h
+ * @author Omegaki113r
+ * @date Friday, 31st May 2024 11:44:39 pm
+ * @copyright Copyright <<projectCreationYear>> - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: inc
+ * File Name: OmegaEmbedKernel.h
+ * File Created: Friday, 31st May 2024 11:44:39 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Friday, 31st May 2024 11:45:19 pm
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright <<projectCreationYear>> - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#ifndef __OMEGA_EMBED_KERNEL_H__
+#define __OMEGA_EMBED_KERNEL_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
